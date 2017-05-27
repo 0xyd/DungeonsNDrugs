6794a070d22d01106e7af52bdb0fc313b10865eb
yd <yd@KCYs-MBA.local>
2017-05-27 17:53:36 +0800

    update

--- schema/build_dnd_schema.py
+++ schema/build_dnd_schema.py
@@ -6,17 +6,23 @@ if __name__ == '__main__':
 	crsr = conn.cursor()
 	crsr.execute(
 		'CREATE TABLE user (user_id text, age real, health real, '
-		'money real, evil real, ch_status)')
+		'money real, evil real, ch_status text, city text)')
 
 	crsr.execute(
 		'CREATE TABLE symptoms (user_id text, disease text, '
 		'year real, damage real)')
 	
 	crsr.execute(
-		'CREATE TABLE items (user_id text, name text, value real, weight real)')
+		'CREATE TABLE items (user_id text, name text, weight real)')
 
 	crsr.execute(
 		'CREATE TABLE drug_symptom (drug text, symptom text, year real)')
+
+	crsr.execute(
+		'CREATE TABLE drug_price (drug text, drug_price real)')
+
+	crsr.execute(
+		'CREATE TABLE crime_record (user_id text, crime_name text)')
 
 	conn.commit()
 	conn.close()
