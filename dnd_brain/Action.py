d4ba0a8f712356b3ec3f514b4ea0a4ecd7844f7f
yd <yd@KCYs-MBA.local>
2017-05-27 09:46:29 +0800

    Update

--- dnd_brain/Action.py
+++ dnd_brain/Action.py
@@ -1,5 +1,18 @@
 # -*- coding: UTF-8 -*-
 import sqlite3
+from random import randint
+from Events import GodDecides, JudgeEvent, PoliceEvent, GangsterEvent, DiseaseStrokeEvent
+
+def standarlise_output(clsmeth):
+	# Return Value Format:
+	# (char_state <dict>, story_message <str>)
+	def _wrap(cls, user_id):
+		rtn = clsmeth(cls, user_id)
+		return {
+			'character': rtn[0], 
+			'message'  : rtn[1]
+		}
+	return _wrap
 
 class Action():
 
@@ -8,6 +21,7 @@ class Action():
 	record = None
 
 	@classmethod
+	@standarlise_output
 	def check_saved_game(cls, user_id):
 		
 		result = cls.cursor.execute(
@@ -15,52 +29,101 @@ class Action():
 
 		cls.record = result.fetchone()
 		if cls.record:
+			cls.record = {
+				'user_id': cls.record[0],
+				'age'    : cls.record[1],
+				'health': cls.record[2],
+				'money' : cls.record[3],
+				'evil'  : cls.record[4],
+				'identity': cls.record[5]
+			}
 
-			return '歡迎你回到這個遊戲，想要繼續嗎？'
+			return (cls.record, '歡迎你回到這個遊戲，想要繼續嗎？')
 		else:
-			return '你第一次參與這項遊戲，想要開始毒與地下城嗎？'
+			return ({}, '你第一次參與這項遊戲，想要開始毒與地下城嗎？')
 
 	@classmethod
+	@standarlise_output
 	def start_game(cls, user_id):
 
 		if cls.record:
 			
-			return '玩家%s現年%s歲，健康為%s，目前擁有%s元，惡名為%s，身份是名%s' % (
-				str(cls.record[0]), str(cls.record[1]), str(cls.record[2]),
-				str(cls.record[3]), str(cls.record[4]), cls.record[5].encode('utf8'))
-				
+			return (cls.record,
+				'玩家%s現年%s歲，健康為%s，目前擁有%s元，惡名為%s，身份是名%s' % (
+				str(cls.record['user_id']), str(cls.record['age']), str(cls.record['health']),
+				str(cls.record['money']), str(cls.record['evil']), cls.record['identity'].encode('utf8')))
+		
+		# Create a new identity for the new user.	
 		else:
+			cls.record = {
+				'user_id': str(user_id),
+				'age':  16,
+				'health': 100,
+				'money' : 10000,
+				'evil'  : 0,
+				'identity': '少年'
+			}
+
 			result = cls.cursor.execute(
 				'''INSERT INTO user VALUES (%s, 16, 100, 10000, 0, '少年')'''
 				% str(user_id))
+
 			cls.conn.commit()
 
-			return '你是一個十六歲少年，.... 你目前有10000元，健康值為100，身世清白'
+			return (cls.record, '你是一個十六歲少年，.... 你目前有10000元，健康值為100，身世清白')
 
 	@classmethod
-	def read_save_game(cls, user_id):
+	def save_game(cls, user_status):
+
+		cls.cursor.execute(
+			'''UPDATE user SET age = %s, health = %s, money = %s, evil = %s WHERE user_id=''' 
+			% (str(user_status['age']), str(user_status['health']), str(user_status['money']), 
+				str(user_status['evil']), user_status['user_id']))
+
+		cls.conn.commit()
+
 		pass
 
 	@classmethod
-	def check_status(cls, user_id):
+	def check_status(cls, user_status):
 		pass
 
 	@classmethod
-	def throw_dice(cls, user_id):
+	def throw_dice(cls, user_status):
+		return randint(1, 12)
+
+	@classmethod
+	def travel(cls, user_status):
 		pass
 
 	@classmethod
-	def choose_city():
+	def buy_drugs(cls, user_status):
 		pass
 
-	def get_event():
+	@classmethod
+	def sell_drugs(cls, user_status, transaction):
+		god_decision = GodDecides(user_status)
+		if god_decision[0]:
+			return cls.gain_money(user_status, transaction)
+		else:
+			return god_decision
+
+	@classmethod
+	def gain_money(cls, user_status, transaction):
+		income = 0
+		for drug, item, price in transaction:
+			income = item * price
+		user_status['money'] += income
+		return (user_status, '共獲得%s元' % str(user_status['money']))
+
+	@classmethod
+	def get_rand_evt(cls, user_status):
 		pass
 
-	def buy_drugs():
+	@classmethod
+	def conduct_darksocial(cls, user_status):
 		pass
 
-	def sell_drugs():
-		pass
 
 
 
--- dnd_brain/BotMind.py
+++ dnd_brain/BotMind.py
@@ -5,20 +5,21 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 
 from Action import Action
-from setting import ACCESS_TOKEN
+from setting import ACCESS_TOKEN, VERIFIED_TOKEN
 from MsgParser import MsgParser
 
 app = Flask(__name__)
 dndbot = Bot(ACCESS_TOKEN)
 
 # 20170525 Y.D.: User's game state.
-USER_STATE = 'NULL'
+USER_STATE = 'NULL' # Player's Game State
+CHAR_STATE = {}     # Player's Character State
 
 @app.route('/', methods = ['GET'])
 def verify():
 	# Webhook verification
 	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == ACCESS_TOKEN:
+		if not request.args.get("hub.verify_token") == VERIFIED_TOKEN:
 			return "Failed validation. Make sure the validation tokens match.", 403
 		return request.args["hub.challenge"], 200
 	return "Hello", 200
@@ -30,15 +31,21 @@ def listen():
 	recipient_id = message.get_sender()
 	message_text = message.get_text()
 	
-	# Bot receives message from facebook page.
-	if message.get_msg_type() == 'page' and recipient_id:
+	# Bot receives message from facebook page and only respond to human message.
+	if message.get_msg_type() == 'page' and message.is_echo() == False:
+
 		global USER_STATE
+		global CHAR_STATE
 		
 		## Basic Game Operation
 		# Check record to start a new game or continue an old one.
 		if USER_STATE == 'NULL':
+			
+			act_result = Action.check_saved_game(recipient_id)
+			respond    = act_result['message']
+			CHAR_STATE = act_result['character']
 			USER_STATE = 'READY'
-			respond = Action.check_saved_game(recipient_id)
+
 			dndbot.send_text_message(recipient_id, respond)
 
 		# Users leave game without playing
@@ -49,18 +56,76 @@ def listen():
 		# Game running
 		elif USER_STATE == 'READY' and message_text == u'是':
 			USER_STATE = 'RUNNING'
-			respond = Action.start_game(recipient_id)
+			act_result = Action.start_game(recipient_id)
+			respond    = act_result['message']
+			CHAR_STATE = act_result['character']
 			dndbot.send_text_message(recipient_id, respond)
 			
 		# Game Ends
-		elif USER_STATE == 'RUNNING' and message_text == u'掰':
+		elif (USER_STATE == 'RUNNING' or USER_STATE == 'DECIDING') and message_text == u'掰':
 			USER_STATE = 'NULL'
+			Action.save_game(CHAR_STATE)
 			dndbot.send_text_message(recipient_id, '遊戲已經儲存，歡迎下次再來！')
 
-		## 	
-		
+		## MUST GO THROUGH PROCESS
+
+		## The Processes while gaming 
+		else:
+			make_decision(recipient_id, message_text)
+			
 
 	return 'ok', 200
+
+
+def make_decision(recipient_id, message_text):
+
+	global USER_STATE
+	global CHAR_STATE
+	global dndbot
+	
+	if USER_STATE == 'DECIDING':
+
+		# 1: 闖蕩江湖
+		# 2: 販賣毒品
+		# 3: 購買毒品
+		# 4: 移動
+		if message_text == '1':
+			dndbot.send_text_message(recipient_id, '開始行動1')
+
+		elif message_text == '2':
+			dndbot.send_text_message(recipient_id, '開始行動2')
+			### For testing purpose
+			### TODO: Select Drugs to sell
+			transaction = [('安非他命', 4000, 2)]
+			###
+			action_result = Action.sell_drugs(CHAR_STATE, transaction)
+
+			if action_result[0]:
+				come_across_randevt(recipient_id, action_result[1])
+				# dndbot.send_text_message(recipient_id, action_result[1])
+
+		elif message_text == '3':
+			dndbot.send_text_message(recipient_id, '開始行動3')
+		elif message_text == '4':
+			dndbot.send_text_message(recipient_id, '開始行動4')
+		
+		USER_STATE = 'RUNNING'	
+
+	elif USER_STATE == 'RUNNING':
+		dndbot.send_text_message(
+			recipient_id, '選擇行動:\n 1) 闖蕩江湖\n 2) 販賣毒品\n 3) 購買毒品\n 4) 移動')
+
+		USER_STATE = 'DECIDING'
+
+
+def come_across_randevt(recipient_id, event):
+
+	global dndbot
+
+	dndbot.send_text_message(recipient_id, event)
+	#### Have to display view for users to play dice
+	dndbot.send_text_message(recipient_id, '請躑骰子')
+
 
 
 if __name__ == '__main__':
