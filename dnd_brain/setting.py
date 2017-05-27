3527efceb0bb97f81569174de5ee5e3ec18482fd
yd <yd@KCYs-MBA.local>
2017-05-28 06:01:01 +0800

    code update

--- dnd_brain/setting.py
+++ dnd_brain/setting.py
@@ -1,5 +1,29 @@
-ACCESS_TOKEN = "EAAKSXEp9RZBMBAMibRJl1OaK3F5IB0YfW1ejdjK1q0ltQjOUVzygpxI9pJmuseodFEgBKxZAYA6UbrJ9lmBXztkIosmZBGnmtzf1WEViT4fPQeP5ZCTHH10yYwraoXh9QXDbnI56jnDqbAQWXOuKSXPN8uZBcTAXrF1jiWXJmoQZDZD"
+# -*- coding: UTF-8 -*-
+ACCESS_TOKEN   = "EAAKSXEp9RZBMBAMibRJl1OaK3F5IB0YfW1ejdjK1q0ltQjOUVzygpxI9pJmuseodFEgBKxZAYA6UbrJ9lmBXztkIosmZBGnmtzf1WEViT4fPQeP5ZCTHH10yYwraoXh9QXDbnI56jnDqbAQWXOuKSXPN8uZBcTAXrF1jiWXJmoQZDZD"
+VERIFIED_TOKEN = "bee9ae84b20fc9a18bcff7e6307633e40c4734cada6f3f0f464e6e5fa5eed670"
 
-COUNTIES = {
-	
-}
+MAIN_ACTIONS = [{
+	"content_type" : "text",
+	"title" : "1) 闖蕩江湖",
+	"payload":"1",
+	#"image_url" : ""
+	},
+	{
+	"content_type" : "text",
+	"title" : "2) 販賣毒品",
+	"payload":"2",
+	#"image_url" : ""
+	},
+	{
+	"content_type" : "text",
+	"title" : "3) 購買毒品",
+	"payload":"3",
+	#"image_url" : ""
+    },
+    {
+    "content_type" : "text",
+    "title" : "4) 移動",
+    "payload":"4",
+    #"image_url" : ""
+}]
+
