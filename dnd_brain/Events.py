0a9ab78a843462f72d27fa860abc36a6616d0b53
yd <yd@KCYs-MBA.local>
2017-05-28 10:20:23 +0800

    update code

--- dnd_brain/Events.py
+++ dnd_brain/Events.py
@@ -178,11 +178,12 @@ class PoliceEvent():
 	def show_event(self):
 
 		if self.event == '臨檢事件':
-			return ('開車遭臨檢:', '開車行經中山路，遇到前方有警員設路障盤查，前方排隊車龍還長，心中盤算究竟該如何是好...》', 'http://i.imgur.com/NKAEkcT.png')
+			return ('開車遭臨檢', '開車行經中山路，遇到前方有警員設路障盤查，前方排隊車龍還長，心中盤算究竟該如何是好...》', './public/news3.png')
 		elif self.event == '檢調搜索':
-			return ("檢調搜索", "你因為事業越做越大，開始受到警方關注，警方除佈下人力跟監，亦順利聲請通訊監察，而你卻毫無察覺...某日，尖銳的電鈴聲劃破早晨的寧靜，警察：「我們是偵查隊刑警，依檢字106年0000689號搜索票執行搜索，請開門配合搜索，謝謝。」驚惶失措的你，只有幾秒反應時間...", "http://i.imgur.com/P0EVkYl.png")
-		else:
-			return "其他事件"
+			return ("檢調搜索", "你因為事業越做越大，開始受到警方關注，警方除佈下人力跟監，亦順利聲請通訊監察，而你卻毫無察覺...某日，尖銳的電鈴聲劃破早晨的寧靜，警察：「我們是偵查隊刑警，依檢字106年0000689號搜索票執行搜索，請開門配合搜索，謝謝。」驚惶失措的你，只有幾秒反應時間...", "./public/news1.png")
+		elif self.event == '刑警攻堅':
+			return ("刑警攻堅", "", "./public/news2.png")
+
 
 	def show_dice(self):
 
@@ -190,15 +191,15 @@ class PoliceEvent():
 			return "擲骰子:\n1~4  無處可逃，馬上被員警發現，並當場查獲毒品，立刻沒收並上銬帶回警局\n5~8  於前方路口迴轉加速逃逸，但立刻遭警網包圍，當場查獲毒品並帶回警局偵詢\n9~12  僅是酒測臨檢，故作鎮定順利通過"
 		elif self.event == '檢調事件':
 			return "擲骰子:\n1~5  突如其來的狀況，驚嚇的雙腿一軟，只能痴呆的任由警方搜索出藏匿的毒品及現金，但這就是違法的必然下場。\n6~11   你不願乖乖就範，立刻將門關上上鎖，迅速逃跑，自以為能從防火巷逃跑，惟警方早就設下天羅地網，你就如同甕中之鱉，縱奮力掙扎，上銬帶回是你唯一宿命\n12  你剛好在外過夜，僥倖地逃過被逮捕的命運，但屋內的毒品及現金盡遭扣押  "
-		else:
-			return "其他事件的骰子條件"
+		elif self.event == '刑警攻堅':
+			return " "
 
 	def make_result(self, dice_value):
 
 		if self.event == '臨檢事件':
 			return self._meet_police(dice_value)
 		elif self.event == '檢調搜索':
-			pass
+			return self._raid(dice_value)
 		elif self.event == '刑警攻堅':
 			pass
 
@@ -221,6 +222,9 @@ class PoliceEvent():
 			return (150, -50000, -7, 0,'你不願乖乖就範，立刻將門關上上鎖，迅速逃跑，自以為能從防火巷逃跑，惟警方早就設下天羅地網，你就如同甕中之鱉，縱奮力掙扎，上銬帶回是你唯一宿命', True)
 		else:
 			return (175, -50000,  0, 0,'你剛好在外過夜，僥倖地逃過被逮捕的命運，但屋內的毒品及現金盡遭扣押', False) 
+
+	def _police_attack(self, dice_value):
+		pass
 
 
 ## Gangster Event
@@ -254,16 +258,24 @@ class GangsterEvent():
 	def show_event(self):
 
 		if self.event == '黑幫霸凌':
-			return ('黑幫霸凌', "身為校園邊緣人的你，加入幫派後依然沒有例外，22k幫向以財力作為地位高低衡量，窮困潦倒的你，連菜鳥都把你當小弟使喚，面對如此困境，你決定...", "http://i.imgur.com/rycGQRj.png")
-		else:
-			return ("其他事件", "其他事件描述")
+			return ('黑幫霸凌', "身為校園邊緣人的你，加入幫派後依然沒有例外，22k幫向以財力作為地位高低衡量，窮困潦倒的你，連菜鳥都把你當小弟使喚，面對如此困境，你決定...", "./public/news8.png")
+		elif self.event == '搶奪地盤':
+			return ('搶奪地盤', '販賣毒品之利益驚人，終引起幫派間之械鬥，互爭地盤，你在這場爭奪戰中，不幸遭對方幫派擄走以作為交涉籌碼，遭綁架斷水斷食的你該…', './public/news6.png')
+		elif self.event == '仇家尋仇':
+			return ('仇家尋仇', '你初出江湖，對於道上規矩多有不知，於販賣毒品時，偶有不慎侵擾他人生意地盤。仇家們無不虎視眈眈，等待機會將所受損害加倍奉還...', './public/news4.png')
+		elif self.event == '遭到背叛':
+			return ('遭到背叛', '', './public/news5.png')	
 
 	def show_dice(self):
 
 		if self.event == '黑幫霸凌':
 			return "擲骰子:\n 1 怨嘆不是含著金湯匙出身的富二代，決定砍掉重練\n 2~8   任命不願抵抗，生無大志只求小確幸，過一日算一日，\n9~10  忍辱負重，持續耕耘，等待翻身的一天\n11~12  奮力抵抗體制，雖歷經一番掙扎、多了幾道傷疤，但終獲尊重 "
-		else:
-			return "其他事件的骰子條件"
+		elif self.event == '搶奪地盤':
+			return "擲骰子:\n 1  慘遭撕票，淪為幫派間利益爭奪的犧牲品。\n2~7  以讓出地盤作為代價而獲得釋放，重獲自由很棒，但卻也損失了獲益最豐的地段。\n8~11    幫派小弟以武力強行將你救出，惟你早以飽受折磨，元氣大傷。\n12     你順利趁綁匪熟睡之際偷溜，並成功發起反擊，振興幫派版圖。"
+		elif self.event == '仇家尋仇':
+			return "擲骰子:\n 1~4  深夜外出購買鹹酥雞時，遭仇家圍堵，以箱型車強行帶至深山毆打，並帶走全身衣物及現金，身心飽受巨大傷害。 \n 5~7   仇家在外四處張揚你賣的是假貨等不實訊息，造成你連續幾個月的慘澹生意。 \n 8~10  你的轎車車輪被戳洞、車身被刮花，半夜時窗戶亦遭不明人士丟擲石塊砸破，夜不安眠，精神緊繃。\n11~12    對於會遭報復之事，你早已有所防範，是除住處被噴漆及灑冥紙外，並無其他損害。"
+		elif self.event == '遭到背叛':
+			return ""
 
 	def make_result(self, dice_value):
 
@@ -281,24 +293,28 @@ class GangsterEvent():
 	def _be_bully(self, dice_value):
 
 		if dice_value == 1:
-			return (0, 0, -100, 0, '怨嘆不是含著金湯匙出身的富二代，決定砍掉重練', False)
-		elif dice_value < 8:
-			money_loss = -100 * dice_value
-			return (0, money_loss, -15, 0, '任命不願抵抗，生無大志只求小確幸，過一日算一日', False)
-		elif dice_value < 10:
+			return (-500, 0, -100, 0, '怨嘆不是含著金湯匙出身的富二代，決定砍掉重練', False)
+		elif dice_value < 9:
+			return (-200, 0, -15, 0, '任命不願抵抗，生無大志只求小確幸，過一日算一日', False)
+		elif dice_value < 11:
 			return (0, 0, 0, 0, '忍辱負重，持續耕耘，等待翻身的一天', False)
 		else:
-			return (20, 0, 0, 0, '奮力抵抗體制，雖歷經一番掙扎、多了幾道傷疤，但終獲尊重', False)
+			return (10, 20000, 0, 0, '奮力抵抗體制，雖歷經一番掙扎、多了幾道傷疤，但終獲尊重', False)
 
 	def _be_betrayed(self):
 
 		return (0,0,0, '遭到背叛', False)
 
 	def _be_kinapped(self, dice_value):
-		if dice_value > 4:
-			return (0, 0, 0, 0,'成功掙脫且逃逸', False)
+		
+		if dice_value == 1:
+			return (0, 0, -100, 0,'慘遭撕票，淪為幫派間利益爭奪的犧牲品。', False)
+		elif dice_value < 8:
+			return (0, -40000, -10, 0, '以讓出地盤作為代價而獲得釋放，重獲自由很棒，但卻也損失了獲益最豐的地段。', False)
+		elif dice_value < 12:
+			return (5, -10000, -15, 0, '幫派小弟以武力強行將你救出，惟你早以飽受折磨，元氣大傷。', False)
 		else:
-			return (0, 0, 0, 0,'支付贖金。', False)
+			return (20, 25000, -3, 0,'你順利趁綁匪熟睡之際偷溜，並成功發起反擊，振興幫派版圖。。', False)
 
 	def _fight_spots(self, dice_value):
 
@@ -309,7 +325,14 @@ class GangsterEvent():
 
 	def _be_revenged(self, dice_value):
 
-		return (0, 0, 0, 0,'仇家尋仇', False)
+		if dice_value < 5:
+			return (-10, -5000, -10, 0, '深夜外出購買鹹酥雞時，遭仇家圍堵，以箱型車強行帶至深山毆打，並帶走全身衣物及現金，身心飽受巨大傷害。', False)
+		elif dice_value < 8:
+			return (-20, -40000, 0, 0, "仇家在外四處張揚你賣的是假貨等不實訊息，造成你連續幾個月的慘澹生意。", False)
+		elif dice_value < 11:
+			return (0, -20000, -3, 0, "你的轎車車輪被戳洞、車身被刮花，半夜時窗戶亦遭不明人士丟擲石塊砸破，夜不安眠，精神緊繃。", False)
+		else:
+			return (0, -5000, -1, 0, "對於會遭報復之事，你早已有所防範，是除住處被噴漆及灑冥紙外，並無其他損害。", False)
 		
 
 class DiseaseStrokeEvent():
